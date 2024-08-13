df02a93c90d2098208b50db848fbad695d575395
fardinfatehpour <fardin8923@gmail.com>
2024-08-13 17:30:09 +0330

    v.1.0.0

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import { Col, Row, Container } from "react-bootstrap";
+
+const App = () => {
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1 className="TxtWelcome">welcome</h1>
+        </Col>
+        <Col>
+        <label >username:</label>
+          <input type="text" className="search" />
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default App;
