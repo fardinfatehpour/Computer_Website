201ae97b02d52993cfc08bd91fe8bef3aaf9850a
fardinfatehpour <fardin8923@gmail.com>
2024-08-16 23:20:10 +0330

    1.0.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import "./App.css";
 import { Col, Row, Container, Button } from "react-bootstrap";
-import person from './assets/person.png'
+import person from "./assets/person.png";
 
 const App = () => {
   return (
     <>
       <Container>
         <Row>
-          <Col>
-            <input type="button"><img src={person}/></input>
+          <Col className="col-3.5">
+            <button id="sign">
+              <img src={person} alt="person"id="person"align="right"/>
+              <h6 align="center"id="signin">ورود</h6>
+              <h6 align="left"id="signup">ثبت نام</h6>
+            </button>
+          </Col>
+          <Col className="col-5">
+            <input type="text" className="search"id="search" />
           </Col>
           <Col>
-            <input type="text" className="search" />
+            <h1 id="Welcome">Welcome</h1>
           </Col>
         </Row>
       </Container>
